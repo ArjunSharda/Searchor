6f0d49d9b1e406154344cf5b1dd55b6cc72e0544
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2022-08-27 13:36:28 -0500

    Create __init__.py

---
+++ Code/Searchor.py/__init__.py
@@ -0,0 +1,49 @@
+def SearchGoogle(query):
+    query = query.replace(" ", "+")
+    query_URL = "https://www.google.com/search?q=" + query
+    return query_URL
+
+def SearchYoutube(query):
+    query = query.replace(" ", "+")
+    query_URL = "https://www.youtube.com/results?search_query=" + query
+    return query_URL
+
+def SearchTwitter(query):
+    query = query.replace(" ", "+")
+    query_URL = "https://twitter.com/search?q=" + query
+    return query_URL
+
+def SearchWikipedia(query):
+    query = query.replace(" ", "+")
+    query_URL = "https://en.wikipedia.org/wiki/" + query
+    return query_URL
+
+def SearchDictionary(query):
+    query = query.replace(" ", "+")
+    query_URL = "https://www.dictionary.com/browse/" + query
+    return query_URL
+
+def SearchYahoo(query):
+    query = query.replace(" ", "+")
+    query_URL = "https://search.yahoo.com/search?p=" + query
+    return query_URL
+
+def SearchAmazon(query):
+    query = query.replace(" ", "+")
+    query_URL = "https://www.amazon.com/s?k=" + query
+    return query_URL
+
+def SearchAlibaba(query):
+    query = query.replace(" ", "+")
+    query_URL = "https://www.alibaba.com/products?q=" + query
+    return query_URL
+
+def SearchBing(query):
+    query = query.replace(" ", "+")
+    query_URL = "https://www.bing.com/search?q=" + query
+    return query_URL
+
+def SearchDuckDuckGo(query):
+    query = query.replace(" ", "+")
+    query_URL = "https://duckduckgo.com/?q=" + query
+    return query_URL
