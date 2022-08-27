4f85dd2103bb8dc3763217f4157a4a443bba8e29
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2022-08-27 13:35:16 -0500

    Create setup.py

---
+++ Code/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 2 - Pre-Alpha',
+  'Intended Audience :: Developers',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.8',
+  'Programming Language :: Python :: 3.9',
+  'Programming Language :: Python :: 3.10'
+]
+ 
+setup(
+  name='Searchor.py',
+  version='1.0.0',
+  description='Math package',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://github.com/ArjunSharda/Mathician',  
+  author="Arjun Sharda",
+  author_email='sharda.aj17@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Searchor.py', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
