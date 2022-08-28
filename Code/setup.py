8beb6a5e9948495c2186b49860c2ca4e9865df91
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2022-08-28 05:39:14 -0500

    Update setup.py

--- Code/setup.py
+++ Code/setup.py
@@ -12,7 +12,7 @@ classifiers = [
  
 setup(
   name='Searchor',
-  version='1.0.5',
+  version='1.1.0',
   description='Math package',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='https://github.com/ArjunSharda/Searchor',  
