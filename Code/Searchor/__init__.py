aceada3270e86a5341c30c3d3b9bc515d532e857
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2022-09-03 11:15:19 -0500

    Update __init__.py

--- Code/Searchor/__init__.py
+++ Code/Searchor/__init__.py
@@ -58,3 +58,13 @@ def SearchGitHub(query):
   query = query.replace(" ", "+")
   query_URL = "https://github.com/search?q=" + query
   return query_URL
+
+def custom(query, queryurl, spacingsymbol):
+  query = query.replace(" ", spacingsymbol)
+  query_URL = queryurl + query
+  return query_URL
+
+def SearchApple(query):
+  query = query.replace(" ", "-")
+  query_URL = "https://apple.com/search/" + query
+  return query_URL
