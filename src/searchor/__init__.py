30e56b0d4ded74d8067c759bd38d454c31d2a618
logic-404 <awesomesinghtanvir@gmail.com>
2022-10-01 02:29:07 +0530

    Added Ebay Search query

--- src/searchor/__init__.py
+++ src/searchor/__init__.py
@@ -38,6 +38,7 @@ class Engine:
     Walmart = "https://www.walmart.com/search?q={query}"
     Youtube = "https://www.youtube.com/results?search_query={query}"
     Yahoo = "https://search.yahoo.com/search?p={query}"
+    Ebay = "https://www.ebay.com/sch/{query}"
 
 #search function    
 def search(query, engine=Engine.Google):
