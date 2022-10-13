f6d3f24fc028c662af184d84683a9cef9d538f4c
tyboro2002 <97916632+tyboro2002@users.noreply.github.com>
2022-10-13 22:34:33 +0200

    added search bunch example

---
+++ examples/search bunch.py
@@ -0,0 +1,7 @@
+from searchor import Engine
+print(Engine.Amazon.search("Hello, World!"))
+print(Engine.Bing.search("Hello World!"))
+print(Engine.DuckDuckGo.search("Hello, World!"))
+print(Engine.Google.search("Hello, World!"))
+print(Engine.Wikipedia.search("Hello, World!"))
+print(Engine.Yahoo.search("Hello, World!"))
