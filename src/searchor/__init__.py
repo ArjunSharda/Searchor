61ca89c1a1e262dc5727157d7dec8f1842688531
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2023-02-28 08:05:12 -0600

    Update __init__.py

--- src/searchor/__init__.py
+++ src/searchor/__init__.py
@@ -144,7 +144,7 @@ class Information(Enum):
     else:
         return "Error: Could not retrieve information"
 
-def web_scraper(url):
+def scrape(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
